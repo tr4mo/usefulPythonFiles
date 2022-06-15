06e6e39b516f1fa8be0db41c1b1bc3c7a3e2f5a0
Matteo <82446643+tr4mo@users.noreply.github.com>
2022-06-15 16:49:45 +0200

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -11,13 +11,20 @@
 #                                                 #
 # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+# This is the number you want to elaborate
 n = 100
+
+# This loop iterates in a range 1 to n
 for i in range(1,n+1):
+    # If the number is divisible by 3 and by 5, print fizzbuzz
     if i % 3 == 0 and i % 5 == 0:
         print("fizzbuzz")
+    # If the number is divisible only by 3, print fizz
     elif i % 3 == 0:
         print("fizz")
+    # If the number is divisible only by 5, print buzz
     elif i % 5 == 0:
         print("buzz")
+    # If the number is not divisible by 3 or by 5, print the actual number
     else:
         print(i)
