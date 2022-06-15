e3babadacc02f3877545cc68ee9269d69ad48523
Matteo Tramontina <82446643+tr4mo@users.noreply.github.com>
2022-06-15 16:08:40 +0200

    Update createTxtFile.py

--- createTxtFile.py
+++ createTxtFile.py
@@ -1,14 +1,15 @@
-# # # # # # # # # # # # # # # # # # # # # # # # #
-#   Created by Matteo Tramontina                #
-#   Date: 15 June 2022                          #
-#   Version: 1.0                                #
-#   Github: https://github.com/tr4mo            #
-#   Email: matteo.tramontina@outlook.com        #
-#                                               #
-#   You can use this program for free, just     #
-#   credit me if you use it for public purposes #
-#                                               #
-# # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                 #
+#   Created by Matteo Tramontina                  #
+#   Date: 15 June 2022                            #
+#   Version: 1.0                                  #
+#   Github: https://github.com/tr4mo              #
+#   Email: matteo.tramontina@outlook.com          #
+#                                                 #
+#   You can use this program for free, just       #
+#   credit me if you use it for public purposes   #
+#                                                 #
+# # # # # # # # # # # # # # # # # # # # # # # # # #
 
 string = input("Type your string: ")
 
